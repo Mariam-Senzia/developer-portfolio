e7fb8c9d361b897c1e15b4619229fecb270155ef
Mariam Senzia <mariamsenzia@gmail.com>
2025-07-19 21:30:25 +0300

    Added reach out and footer components on the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,8 @@
 import AboutHero from "../components/about/AboutHero";
 import ExperienceAndEducation from "../components/about/ExperienceAndEducation";
+import Footer from "../components/home/Footer";
 import Navbar from "../components/home/Navbar";
+import ReachOut from "../components/home/ReachOut";
 
 const About = () => {
   return (
@@ -8,6 +10,8 @@ const About = () => {
     <Navbar />
     <AboutHero />
     <ExperienceAndEducation />
+    <ReachOut />
+    <Footer />
   </>
   );
 };
